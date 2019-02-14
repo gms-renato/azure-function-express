ca8196056e2823f87c12feddcfabe3bf25ef462d
Swapnil Bhosale <swapnil.bhosale.ext@siemens.com>
2019-02-14 11:50:19 +0530

    1)update formatting

--- test/IncomingMessage.test.js
+++ test/IncomingMessage.test.js
@@ -11,10 +11,10 @@ describe("IncomingMessage", () => {
   it("Should work", () => {
 
     const context = {
-      bindings: {
-        req: {
-          originalUrl: "https://foo.com/bar",
-          headers: { "x-forwarded-for": "192.168.0.1:57996" }
+      bindings : {
+        req : {
+          originalUrl : "https://foo.com/bar",
+          headers     : { "x-forwarded-for": "192.168.0.1:57996" }
         }
       }
     };
@@ -24,10 +24,10 @@ describe("IncomingMessage", () => {
     req.socket.destroy();
 
     expect(req).toMatchObject({
-      url: "https://foo.com/bar",
-      connection: {
-        encrypted: true,
-        remoteAddress: "192.168.0.1"
+      url         : "https://foo.com/bar",
+      connection  : {
+        encrypted     : true,
+        remoteAddress : "192.168.0.1"
       }
     });
   });
@@ -35,9 +35,9 @@ describe("IncomingMessage", () => {
   it("Should work with no headers", () => {
 
     const context = {
-      bindings: {
-        req: {
-          originalUrl: "http://foo.com/bar"
+      bindings : {
+        req : {
+          originalUrl : "http://foo.com/bar"
         }
       }
     };
@@ -47,10 +47,10 @@ describe("IncomingMessage", () => {
     req.socket.destroy();
 
     expect(req).toMatchObject({
-      url: "http://foo.com/bar",
-      connection: {
-        encrypted: false,
-        remoteAddress: undefined
+      url         : "http://foo.com/bar",
+      connection  : {
+        encrypted     : false,
+        remoteAddress : undefined
       }
     });
   });
@@ -58,14 +58,14 @@ describe("IncomingMessage", () => {
   it("Should work with a full native context object", () => {
 
     const context = {
-      invocationId: "f0f6e586-0b79-4407-aa53-97919f45eba5",
-      bindingData: { foo: "bar" },
-      bindings: {
-        req: {
-          originalUrl: "http://foo.com/bar"
+      invocationId : "f0f6e586-0b79-4407-aa53-97919f45eba5",
+      bindingData : { foo: "bar" },
+      bindings : {
+        req : {
+          originalUrl : "http://foo.com/bar"
         }
       },
-      done: () => { }
+      done  : () => {}
     };
 
     const req = new IncomingMessage(context);
@@ -73,10 +73,10 @@ describe("IncomingMessage", () => {
     req.socket.destroy();
 
     expect(req).toMatchObject({
-      url: "http://foo.com/bar",
-      connection: {
-        encrypted: false,
-        remoteAddress: undefined
+      url         : "http://foo.com/bar",
+      connection  : {
+        encrypted     : false,
+        remoteAddress : undefined
       }
     });
 
